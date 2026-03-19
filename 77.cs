7eeef27fdd680873a93b8641c8b9c133cda0f8e8
francescoolivaet32-lang <francesco.olivaet32@gmail.com>
2026-03-19 12:03:05 -0300

    Add files via upload

---
+++ 77.cs
@@ -0,0 +1,119 @@
+﻿int a = 25;
+
+int b = 30;
+
+if (a > b) 
+{
+    Console.WriteLine( "Verdedero");
+}
+
+else { 
+
+    Console.WriteLine("Falso");
+}
+
+int t = 14;
+
+int y= 14;
+
+if (t <= y)
+{
+    Console.WriteLine("Verdadero");
+}
+
+else
+{
+    Console.WriteLine("Falso");
+}
+
+int ab = 0;
+
+int ba = 0;
+
+if (ba == ab)
+{
+    Console.WriteLine("Verdedero");
+}
+
+else
+{
+
+    Console.WriteLine("Falso");
+}
+
+int h = 10;
+
+int i = 10;
+
+if (h != i)
+{
+    Console.WriteLine("Verdedero");
+}
+
+else
+{
+
+    Console.WriteLine("Falso");
+
+
+int o = 8;
+
+int j = 3;
+
+if (o >= j)
+{
+        Console.WriteLine("Verdedero");
+}
+
+else
+{
+        Console.WriteLine("Falso");
+}
+
+
+int q = 15;
+
+int k = 10;
+
+    if (q < k)
+    {
+        Console.WriteLine("Verdedero");
+    }
+
+    else
+    {
+        Console.WriteLine("Falso");
+
+    }
+
+
+
+
+
+
+
+int edad = 16;
+
+if (!(edad >= 18))
+{
+        Console.WriteLine("Verdadero");
+}
+ 
+else
+{
+        Console.WriteLine("Falso");
+}
+
+    int temperatura = 35;
+    bool humedad = false;
+
+    if (temperatura > 30 && humedad == true)
+    {
+        Console.WriteLine("Verdadero");
+
+    }
+
+    else
+    {
+      Console.WriteLine("Falso")
+    } 
