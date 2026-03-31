0e3c30227ba59a75ab2ad996b911f2b97c191caf
francescoolivaet32-lang <francesco.olivaet32@gmail.com>
2026-03-31 13:10:57 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+
+for (int i = 0; i <= 10; i++) {
+
+    Console.WriteLine(i);
+}
+
+
+
+
+for (int i = 0; i <= 5; i++)
+{
+
+    Console.WriteLine("hola mundo");
+}
+
+
+
+for (int i = 0; i <= 20; i=2+i)
+{
+
+    Console.WriteLine(i);
+}
+
+for (int i = 0; i <= 70; i = 7 + i)
+{
+
+    Console.WriteLine(i);
+}
+
+int suma = 0;
+for (int i = 0; i <= 5; i++)
+{
+    suma += i;
+}
+    Console.Write("1+2+3+4+5= " );
+Console.WriteLine(suma);
