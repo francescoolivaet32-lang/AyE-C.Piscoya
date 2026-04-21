485be0cbdfb7083e2ac82edea9faf1a8d90a0519
francescoolivaet32-lang <francesco.olivaet32@gmail.com>
2026-04-21 11:47:00 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿string Bloque1(int i)
+{
+
+    for (int i = 0; i <= 100; i = 3 + i)
+    {
+
+        Console.WriteLine(i);
+    }
+
+
+   
+
+    if (o > 0)
+    {
+        resultado = "Es positivo";
+        return resultado;
+    }
+    else if (n == 0)
+    {
+        resultado = "Es 0";
+        return resultado;
+    }
+
+    else
+    {
+        resultado = "es negativo";
+        return resultado;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
