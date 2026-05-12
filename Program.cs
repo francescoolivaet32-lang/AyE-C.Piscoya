d7ab5c34b42eb06f6c7bba3b4ea24ba29ed43c69
francescoolivaet32-lang <francesco.olivaet32@gmail.com>
2026-05-12 12:28:12 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,108 +1,28 @@
-//ejercicio 1
+﻿// Ejercicio 2
 
-Console.WriteLine("Ingrese una frase");
-string frase = Console.ReadLine();
-string vocales = ("aeiouAEIOU");
-int contador = 0;
+Console.WriteLine("Vamos a sumar todos los numeros ingrsados, se dejaran de sumar cuando el usuario ingrese 0");
+Console.WriteLine("ingrese un numero, ingrese 0 cuando quiera terminar la suma");
+string numeros = Console.ReadLine();
+numeros = numeros + 1;
 
-foreach (char f in frase)
-{
-    foreach (char v in vocales)
-    {
-        if (f == v)
-        {
-            contador = contador + 1;
-            break;
-        }
+if (numeros > 0 ) {
 
-    }
+    Consol
 }
-Console.WriteLine("La cantidad de vocales que tiene es: " + contador);
 
-
-//ejercicio 2
-Console.WriteLine("");
-Console.WriteLine("Ingrese una palabra , la invertiremos");
-String palabra = Console.ReadLine();
-String invertido = "";
-foreach (char l in palabra)
-{
-    invertido = l + invertido;
-}
-Console.WriteLine("");
-Console.WriteLine("La palabra invertida es: " + invertido);
-
-
-//ejercicio 3
-Console.WriteLine("");
-Console.WriteLine("Ingrese un numero");
-string texto = Console.ReadLine();
-int aux = 0;
-int aux2 = 0;
-
-foreach (char n in texto)
-{
-    aux = Convert.ToInt32("" + n);
-    aux2 = aux + aux2;
-}
-Console.WriteLine("");
-Console.WriteLine("La suma es: " + aux2);
-
-
-//ejercicio 4
-Console.WriteLine("");
-Console.WriteLine("Ingrese un texto largo:");
-string frase3 = Console.ReadLine();
-Console.WriteLine("");
-Console.WriteLine("Ingrese una frase prohibida");
-string fraseprohibida = Console.ReadLine();
-Console.WriteLine("");
-Console.WriteLine("Ingrese una frase para reemplazarla");
-string frasereemplazo = Console.ReadLine();
-string nuevafrase = "";
-if (frase3.Contains(fraseprohibida) == true)
-{
-    nuevafrase = frase3.Replace(fraseprohibida, frasereemplazo);
-}
-Console.WriteLine("");
-Console.WriteLine("Esta es la frase: " + nuevafrase);
-
-
-
-//ejercicio 5
-Console.WriteLine("");
-Console.WriteLine("Ingrese su nombre");
-string nombre = Console.ReadLine();
-Console.WriteLine("");
-Console.WriteLine("Ingrese su apellido");
-string apellido = Console.ReadLine();
-string iniciales = "";
-iniciales = nombre[0] + "." + apellido[0];
-Console.WriteLine("");
-Console.WriteLine("Su nombre es: " + nombre);
-Console.WriteLine("");
-Console.WriteLine("Su apellido es: " + apellido);
-Console.WriteLine("");
-Console.WriteLine("Sus iniciales son: " + iniciales);
-
-
-//ejercicio 6
-Console.WriteLine("");
-Console.WriteLine("Ingrese una palabra , identificaremos si es un palindromo:");
-String palindromo = Console.ReadLine();
-palindromo = palindromo.ToLower();
-String invertido2 = "";
-foreach (char j in palindromo)
-{
-    invertido2 = j + invertido2;
-}
-if (palindromo == invertido2)
-{
-    Console.WriteLine("");
-    Console.WriteLine("La palabra '" + palindromo + "' es un palindromo");
-}
 else
 {
-    Console.WriteLine("");
-    Console.WriteLine("La palabra '" + palindromo + "' NO es un palindromo");
+
 }
+
+//Ejercicio 3
+
+
+
+string vocales = ("aeiouAEIOU");
+
+
+
+
+
+
