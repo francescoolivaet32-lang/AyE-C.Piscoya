02d1bd590c7678f98625c1ea77fd265a5882ca04
francescoolivaet32-lang <francesco.olivaet32@gmail.com>
2026-06-04 09:22:13 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("ingrese un numero para generar  la factorial");
+int aceite = Convert.ToInt32(Console.ReadLine());
+
+int xd = 1;
+for (int Thiago = 1; Thiago <= aceite; Thiago++)
+{
+    xd = xd * Thiago;
+
+
+}
+while (aceite < xd)
+{
+    Console.WriteLine("reintenta");
+    if (aceite < 0)
+    {
+
+    }
+    else
+    {
+        Console.WriteLine("Intentelo de nuevo");
+    }
+    aceite = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("el factorial de tu numero es: " + xd);
