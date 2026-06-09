5ab1fd528de30d2973ba312d72dc5a65fae0ee67
francescoolivaet32-lang <francesco.olivaet32@gmail.com>
2026-06-09 11:52:38 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,108 +1,315 @@
-//ejercicio 1
+﻿string[] nombres = new string[100];
+string[] telefonos = new string[100];
 
-Console.WriteLine("Ingrese una frase");
-string frase = Console.ReadLine();
-string vocales = ("aeiouAEIOU");
-int contador = 0;
 
-foreach (char f in frase)
+nombres[0] = "Juan";
+telefonos[0] = "+54 9 11 3845-2910";
+
+nombres[1] = "alvaro";
+telefonos[1] = "+54 9 11 5920-1148";
+
+nombres[2] = "maximo";
+telefonos[2] = "+54 9 341 683-9201";
+
+nombres[3] = "Exequiel";
+telefonos[3] = "+54 9 351 472-8839";
+
+nombres[4] = "perrro malo";
+telefonos[4] = "+54 9 261 599-4321";
+
+nombres[5] = "el turro";
+telefonos[5] = "+54 9 11 2349-8810";
+
+nombres[6] = "Agus";
+telefonos[6] = "+54 9 11 7102-3944";
+
+nombres[7] = "Donato";
+telefonos[7] = "+54 9 341 551-7029";
+
+nombres[8] = "Mama";
+telefonos[8] = "+54 9 351 823-4411";
+
+nombres[9] = "Aaron";
+telefonos[9] = "+54 9 261 314-9582";
+
+nombres[10] = "Pedro";
+telefonos[10] = "+54 9 11 6023-8174";
+
+nombres[11] = "Julio";
+telefonos[11] = "+54 9 11 4918-2039";
+
+nombres[12] = "Santiago";
+telefonos[12] = "+54 9 341 722-1054";
+
+nombres[13] = "Angelo";
+telefonos[13] = "+54 9 351 619-3382";
+
+nombres[14] = "Melisa";
+telefonos[14] = "+54 9 261 402-7719";
+
+nombres[15] = "Katerine";
+telefonos[15] = "+54 9 11 5039-4418";
+
+nombres[16] = "Matheo";
+telefonos[16] = "+54 9 11 3110-8472";
+
+nombres[17] = "Brian";
+telefonos[17] = "+54 9 341 894-2311";
+
+nombres[18] = "Max";
+telefonos[18] = "+54 9 351 505-9923";
+
+nombres[19] = "Pablo";
+telefonos[19] = "+54 9 261 731-5540";
+
+nombres[20] = "Jose";
+telefonos[20] = "+54 9 11 2948-1156";
+
+nombres[21] = "Nicolas";
+telefonos[21] = "+54 9 11 6834-9205";
+
+nombres[22] = "Camargo";
+telefonos[22] = "+54 9 341 415-8832";
+
+nombres[23] = "Luciano";
+telefonos[23] = "+54 9 351 772-1049";
+
+nombres[24] = "Maciel";
+telefonos[24] = "+54 9 261 629-3314";
+
+nombres[25] = "Fran";
+telefonos[25] = "+54 9 11 4028-7751";
+
+nombres[26] = "Andrea";
+telefonos[26] = "+54 9 11 5119-4428";
+
+nombres[27] = "Georgina";
+telefonos[27] = "+54 9 341 394-2305";
+
+nombres[28] = "Carolina";
+telefonos[28] = "+54 9 351 805-9914";
+
+nombres[29] = "Regina";
+telefonos[29] = "+54 9 261 531-5572";
+
+nombres[30] = "Brisa";
+telefonos[30] = "+54 9 11 7948-1123";
+
+nombres[31] = "Lisandro";
+telefonos[31] = "+54 9 11 2834-9261";
+
+nombres[32] = "Roberto";
+telefonos[32] = "+54 9 341 615-8874";
+
+nombres[33] = "Dylan";
+telefonos[33] = "+54 9 351 472-1019";
+
+nombres[34] = "Gloria";
+telefonos[34] = "+54 9 261 329-3351";
+
+nombres[35] = "Alan";
+telefonos[35] = "+54 9 11 5028-7728";
+
+nombres[36] = "Carlos";
+telefonos[36] = "+54 9 11 3119-4462";
+
+nombres[37] = "Martin";
+telefonos[37] = "+54 9 341 794-2381";
+
+nombres[38] = "Joaquin";
+telefonos[38] = "+54 9 351 605-9954";
+
+nombres[39] = "Cielo";
+telefonos[39] = "+54 9 261 431-5510";
+
+nombres[40] = "Dene";
+telefonos[40] = "+54 9 11 6948-1182";
+
+nombres[41] = "Joaquina";
+telefonos[41] = "+54 9 11 5834-9239";
+
+nombres[42] = "Lautaro";
+telefonos[42] = "+54 9 341 815-8811";
+
+nombres[43] = "Ramiro";
+telefonos[43] = "+54 9 351 772-1073";
+
+nombres[44] = "Alex";
+telefonos[44] = "+54 9 261 529-3390";
+
+nombres[45] = "Traverzo";
+telefonos[45] = "+54 9 11 3028-7744";
+
+nombres[46] = "Messi";
+telefonos[46] = "+54 9 11 4119-4417";
+
+nombres[47] = "Cristiano";
+telefonos[47] = "+54 9 341 694-2329";
+
+nombres[48] = "Franchela";
+telefonos[48] = "+54 9 351 505-9982";
+
+nombres[49] = "Dario";
+telefonos[49] = "+54 9 261 731-5531";
+
+nombres[50] = "Paola";
+telefonos[50] = "+54 9 11 2948-1104";
+
+nombres[51] = "Maria Elena";
+telefonos[51] = "+54 9 11 6834-9273";
+
+nombres[52] = "Dardo";
+telefonos[52] = "+54 9 341 415-8890";
+
+nombres[53] = "Homero";
+telefonos[53] = "+54 9 351 772-1015";
+
+nombres[54] = "Marge";
+telefonos[54] = "+54 9 261 629-3342";
+
+nombres[55] = "Bart";
+telefonos[55] = "+54 9 11 4028-7719";
+
+nombres[56] = "Jeffrey Epstein";
+telefonos[56] = "+54 9 11 5119-4483";
+
+nombres[57] = "Adolfo Hitler";
+telefonos[57] = "+54 9 341 394-2354";
+
+nombres[58] = "Donal Trump";
+telefonos[58] = "+54 9 351 805-9928";
+
+nombres[59] = "P. Diddy";
+telefonos[59] = "+54 9 261 531-5517";
+
+nombres[60] = "Mike Tyson";
+telefonos[60] = "+54 9 11 7948-1190";
+
+nombres[61] = "Kim Jong-un";
+telefonos[61] = "+54 9 11 2834-9242";
+
+nombres[62] = "Cristina Fernandez kirchner";
+telefonos[62] = "+54 9 341 615-8819";
+
+nombres[63] = "Maduro";
+telefonos[63] = "+54 9 351 472-1083";
+
+nombres[64] = "Joseph Stanlin";
+telefonos[64] = "+54 9 261 329-3354";
+
+nombres[65] = "Mirtha Legrand";
+telefonos[65] = "+54 9 11 5028-7721";
+
+nombres[66] = "Stephen Hawking";
+telefonos[66] = "+54 9 11 3119-4490";
+
+nombres[67] = "Spider Mad";
+telefonos[67] = "+54 9 341 794-2342";
+
+nombres[68] = "Goku";
+telefonos[68] = "+54 9 351 605-9919";
+
+nombres[69] = "Vegeta";
+telefonos[69] = "+54 9 261 431-5583";
+
+nombres[70] = "Tony Stark";
+telefonos[70] = "+54 9 11 6948-1154";
+
+nombres[71] = "Super Man";
+telefonos[71] = "+54 9 11 5834-9228";
+
+nombres[72] = "Batman";
+telefonos[72] = "+54 9 341 815-8817";
+
+nombres[73] = "Pekka";
+telefonos[73] = "+54 9 351 772-1090";
+
+nombres[74] = "Lanza Fuegos";
+telefonos[74] = "+54 9 261 529-3342";
+
+nombres[75] = "Mosquetera";
+telefonos[75] = "+54 9 11 3028-7715";
+
+int cantidad = 76;
+
+int opcion = 0;
+
+while (opcion != 4)
 {
-    foreach (char v in vocales)
+    Console.WriteLine(" MENU ");
+    Console.WriteLine("1 - Buscar contacto");
+    Console.WriteLine("2 - Agregar contacto");
+    Console.WriteLine("3 - Mostrar contactos");
+    Console.WriteLine("4 - Salir");
+
+    Console.Write("Elegi una opcion: ");
+    opcion = int.Parse(Console.ReadLine());
+
+    Console.WriteLine();
+
+    switch (opcion)
     {
-        if (f == v)
-        {
-            contador = contador + 1;
+        case 1:
+
+            Console.Write("Ingresa el nombre: ");
+            string buscar = Console.ReadLine();
+
+            bool encontrado = false;
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                if (nombres[i].ToLower() == buscar.ToLower())
+                {
+                    Console.WriteLine("Telefono: " + telefonos[i]);
+                    encontrado = true;
+                }
+            }
+
+            if (encontrado == false)
+            {
+                Console.WriteLine("Contacto no encontrado");
+            }
+
             break;
-        }
 
+        case 2:
+
+            Console.Write("Nuevo nombre: ");
+            nombres[cantidad] = Console.ReadLine();
+
+            Console.Write("Nuevo telefono: ");
+            telefonos[cantidad] = Console.ReadLine();
+
+            cantidad++;
+
+            Console.WriteLine("Contacto agregado");
+
+            break;
+
+        case 3:
+
+            Console.WriteLine(" CONTACTOS ");
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.WriteLine(nombres[i] + " - " + telefonos[i]);
+            }
+
+            break;
+
+        case 4:
+
+            Console.WriteLine("Programa terminado");
+
+            break;
+
+        default:
+
+            Console.WriteLine("Opcion invalida");
+
+            break;
     }
-}
-Console.WriteLine("La cantidad de vocales que tiene es: " + contador);
 
+    Console.WriteLine(" ");
 
-//ejercicio 2
-Console.WriteLine("");
-Console.WriteLine("Ingrese una palabra , la invertiremos");
-String palabra = Console.ReadLine();
-String invertido = "";
-foreach (char l in palabra)
-{
-    invertido = l + invertido;
-}
-Console.WriteLine("");
-Console.WriteLine("La palabra invertida es: " + invertido);
-
-
-//ejercicio 3
-Console.WriteLine("");
-Console.WriteLine("Ingrese un numero");
-string texto = Console.ReadLine();
-int aux = 0;
-int aux2 = 0;
-
-foreach (char n in texto)
-{
-    aux = Convert.ToInt32("" + n);
-    aux2 = aux + aux2;
-}
-Console.WriteLine("");
-Console.WriteLine("La suma es: " + aux2);
-
-
-//ejercicio 4
-Console.WriteLine("");
-Console.WriteLine("Ingrese un texto largo:");
-string frase3 = Console.ReadLine();
-Console.WriteLine("");
-Console.WriteLine("Ingrese una frase prohibida");
-string fraseprohibida = Console.ReadLine();
-Console.WriteLine("");
-Console.WriteLine("Ingrese una frase para reemplazarla");
-string frasereemplazo = Console.ReadLine();
-string nuevafrase = "";
-if (frase3.Contains(fraseprohibida) == true)
-{
-    nuevafrase = frase3.Replace(fraseprohibida, frasereemplazo);
-}
-Console.WriteLine("");
-Console.WriteLine("Esta es la frase: " + nuevafrase);
-
-
-
-//ejercicio 5
-Console.WriteLine("");
-Console.WriteLine("Ingrese su nombre");
-string nombre = Console.ReadLine();
-Console.WriteLine("");
-Console.WriteLine("Ingrese su apellido");
-string apellido = Console.ReadLine();
-string iniciales = "";
-iniciales = nombre[0] + "." + apellido[0];
-Console.WriteLine("");
-Console.WriteLine("Su nombre es: " + nombre);
-Console.WriteLine("");
-Console.WriteLine("Su apellido es: " + apellido);
-Console.WriteLine("");
-Console.WriteLine("Sus iniciales son: " + iniciales);
-
-
-//ejercicio 6
-Console.WriteLine("");
-Console.WriteLine("Ingrese una palabra , identificaremos si es un palindromo:");
-String palindromo = Console.ReadLine();
-palindromo = palindromo.ToLower();
-String invertido2 = "";
-foreach (char j in palindromo)
-{
-    invertido2 = j + invertido2;
-}
-if (palindromo == invertido2)
-{
-    Console.WriteLine("");
-    Console.WriteLine("La palabra '" + palindromo + "' es un palindromo");
-}
-else
-{
-    Console.WriteLine("");
-    Console.WriteLine("La palabra '" + palindromo + "' NO es un palindromo");
 }
