670d511275542161e6553195092b2464e855f018
francescoolivaet32-lang <francesco.olivaet32@gmail.com>
2026-07-17 12:52:26 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,154 +1,158 @@
-﻿bool ejercicio1(int n)
+﻿using System.ComponentModel;
+
+try
 {
-
-    if (n == 0)
+    void ejercicio1()
     {
+        int[] numeros = new int[5];
 
-        return false;
-    }
-    else
-    {
-        ejercicio1(n - 1);
-        Console.WriteLine(n);
-        return true;
-    }
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            Console.WriteLine($"Ingrese el numero n°{i + 1}");
 
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Los numeros ingresados fueron: ");
+        foreach (int numero in numeros)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+    ejercicio1();
 }
-ejercicio1(10);
-
-bool ejercicio2(int n)
+catch (System.FormatException)
 {
-
-    if (n == 0)
-    {
-
-        return false;
-    }
-    else
-    {
-
-        Console.WriteLine("¡Hola mundo!");
-        ejercicio2(n - 1);
-        return true;
-    }
-}
-ejercicio2(5);
-bool ejercicio3(int n)
-{
-    if (n > 20)
-    {
-        return false;
-    }
-    else
-    {
-
-        Console.WriteLine(n);
-        ejercicio3(n + 2);
-        return true;
-    }
-}
-ejercicio3(2);
-bool ejercicio4(int n)
-{
-    if (n > 70)
-    {
-        return false;
-    }
-    else
-    {
-        Console.WriteLine(n);
-        ejercicio4(n + 7);
-        return true;
-    }
-}
-ejercicio4(7);
-int aux = 0;
-int ejercicio5(int n)
-{
-    if (n == 0)
-    {
-        return aux;
-    }
-    else
-    {
-        aux = aux + n;
-        ejercicio5(n - 1);
-        return aux;
-    }
+    Console.WriteLine("Ingrese numeros");
 }
 
-aux = ejercicio6(Convert.ToInt32(Console.ReadLine()));
-Console.WriteLine(aux);
-string ejercicio6(string contra)
+void ejercicio2()
 {
-    string secreta = "caine";
+    string[] frutas = { "banana", "pera", "manzana" };
 
-    if(secreta == contra)
-    {
-        Console.WriteLine("La contraseña es correcta");
-        return contra;
-    }
-    else
-    {
-        Console.WriteLine("Intentelo de nuevo");
-        ejercicio6(Console.ReadLine());
-        return contra;
+    Console.WriteLine("Ingrese el nombre de una fruta");
+    string fruta = Console.ReadLine().ToLower();
 
+    for (int i = 0; i < frutas.Length; i++)
+    {
+        if (fruta == frutas[i])
+        {
+            Console.WriteLine($"Su posicion es:{i}");
+            break;
+        }
     }
 }
-ejercicio6(Console.ReadLine());
+ejercicio2();
 
-int ejercicio7(int despegar)
+try
 {
-    if (despegar == 0)
+    void ejercicio3()
     {
-        Console.WriteLine("Listos para despegar");
-        Console.WriteLine("Despegue");
-        return despegar;
-    }
-    else
-    {
-        despegar = despegar - 1;
-        Console.WriteLine(despegar);
-        ejercicio7(despegar);
-        return despegar;
-    }
-}
-ejercicio7(6);
-int ejercicio8(int maquina)
-{
-    Console.WriteLine("Adivine el numero");
-    int numero = Convert.ToInt32(Console.ReadLine());
-    if (numero == maquina)
-    {
-        Console.WriteLine("¡Felicidades! Adivinaste el número.");
-        return maquina;
-    }
-    else
-    {
-        Console.WriteLine("intentelo de vuelta");
-        ejercicio8(maquina);
-        return maquina;
-    }
-}
-ejercicio8(7);
-int acum = 0;
-int ejercicio9()
-{
-    Console.WriteLine("Ingrese un numero");
-    int num = Convert.ToInt32(Console.ReadLine());
-    if(num == 0)
-    {
-        Console.WriteLine("La suma de los numeros es: " + acum);
-        return acum;
-    }
-    else
-    {
-        acum = acum + num;
-        Console.WriteLine("Ingrese otro numero");
-        ejercicio9();
-    }
-    return acum;
+        int[] notas = new int[10];
 
+        int suma_total = 0;
+
+        int promedio = 0;
+
+        for (int i = 0; i < notas.Length; i++)
+        {
+            Console.WriteLine($"Ingrese la nota n°{i + 1}");
+
+            notas[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Los numeros ingresados fueron: ");
+        foreach (int nota in notas)
+        {
+            suma_total = suma_total + nota;
+            promedio = promedio + nota;
+        }
+        Console.WriteLine($"La suma total de las notas es:{suma_total}");
+        Console.WriteLine($"El promedio es:{promedio / 10}");
+    }
+    ejercicio3();
 }
-ejercicio9();
+catch (System.FormatException)
+{
+    Console.WriteLine("Ingrese numeros");
+}
+
+void ejercicio4()
+{
+    int[] temperaturas = { 38, 42, 31, 40, 35, 20, 23, 50, 13, 42, 56, 25 };
+
+    int temp_alta = 0;
+    int temp_baja = temperaturas[0];
+
+    foreach (int temp in temperaturas)
+    {
+        if (temp > temp_alta)
+        {
+            temp_alta = temp;
+        }
+    }
+    Console.WriteLine($"La temperatura mas alta registrada es: {temp_alta}");
+
+    foreach (int tempu in temperaturas)
+    {
+        if (tempu < temp_baja)
+        {
+            temp_baja = tempu;
+        }
+    }
+    Console.WriteLine($"La temperatura mas baja registrada es: {temp_baja}");
+}
+ejercicio4();
+
+void ejercicio5()
+{
+    int[] desordenados = { 38, 42, 31, 40, 35 };
+
+    Console.WriteLine("Ordenados de forma ascendente queda tal que asi: ");
+    for (int pasada = 0; pasada < desordenados.Length; pasada++)
+    {
+        for (int u = 0; u < desordenados.Length - 1; u++)
+        {
+            if (desordenados[u] > desordenados[u + 1])
+            {
+                int temporal = desordenados[u];
+                desordenados[u] = desordenados[u + 1];
+                desordenados[u + 1] = temporal;
+            }
+        }
+    }
+
+    foreach (int numero in desordenados)
+    {
+        Console.Write(numero + " ");
+    }
+}
+ejercicio5();
+
+void ejercicio6()
+{
+    int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+    int pares = 0;
+    int impares = 0;
+
+    Console.WriteLine("Lista");
+    foreach (int nu in numeros)
+    {
+        Console.WriteLine(nu);
+    }
+
+    foreach (int num in numeros)
+    {
+        if (num % 2 == 0)
+        {
+            pares++;
+        }
+        else
+        {
+            impares++;
+        }
+    }
+    Console.WriteLine($"Cant numeros pares: {pares}");
+
+    Console.WriteLine($"Cant numeros impares: {impares}");
+}
+ejercicio6();
