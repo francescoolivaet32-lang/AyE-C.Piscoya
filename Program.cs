ebe37d86600195a53d7da0d15e68d86a2bbd29c5
francescoolivaet32-lang <francesco.olivaet32@gmail.com>
2026-07-17 12:56:08 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,158 +1,135 @@
-﻿using System.ComponentModel;
-
-try
+﻿try
 {
-    void ejercicio1()
+    void programa6()
     {
-        int[] numeros = new int[5];
-
-        for (int i = 0; i < numeros.Length; i++)
+        string[,] matrizVisual = new string[10, 10];
+        for (int fila = 0; fila < matrizVisual.GetLength(0); fila++)
         {
-            Console.WriteLine($"Ingrese el numero n°{i + 1}");
-
-            numeros[i] = int.Parse(Console.ReadLine());
-        }
-        Console.WriteLine("Los numeros ingresados fueron: ");
-        foreach (int numero in numeros)
-        {
-            Console.WriteLine(numero);
-        }
-    }
-    ejercicio1();
-}
-catch (System.FormatException)
-{
-    Console.WriteLine("Ingrese numeros");
-}
-
-void ejercicio2()
-{
-    string[] frutas = { "banana", "pera", "manzana" };
-
-    Console.WriteLine("Ingrese el nombre de una fruta");
-    string fruta = Console.ReadLine().ToLower();
-
-    for (int i = 0; i < frutas.Length; i++)
-    {
-        if (fruta == frutas[i])
-        {
-            Console.WriteLine($"Su posicion es:{i}");
-            break;
-        }
-    }
-}
-ejercicio2();
-
-try
-{
-    void ejercicio3()
-    {
-        int[] notas = new int[10];
-
-        int suma_total = 0;
-
-        int promedio = 0;
-
-        for (int i = 0; i < notas.Length; i++)
-        {
-            Console.WriteLine($"Ingrese la nota n°{i + 1}");
-
-            notas[i] = int.Parse(Console.ReadLine());
-        }
-        Console.WriteLine("Los numeros ingresados fueron: ");
-        foreach (int nota in notas)
-        {
-            suma_total = suma_total + nota;
-            promedio = promedio + nota;
-        }
-        Console.WriteLine($"La suma total de las notas es:{suma_total}");
-        Console.WriteLine($"El promedio es:{promedio / 10}");
-    }
-    ejercicio3();
-}
-catch (System.FormatException)
-{
-    Console.WriteLine("Ingrese numeros");
-}
-
-void ejercicio4()
-{
-    int[] temperaturas = { 38, 42, 31, 40, 35, 20, 23, 50, 13, 42, 56, 25 };
-
-    int temp_alta = 0;
-    int temp_baja = temperaturas[0];
-
-    foreach (int temp in temperaturas)
-    {
-        if (temp > temp_alta)
-        {
-            temp_alta = temp;
-        }
-    }
-    Console.WriteLine($"La temperatura mas alta registrada es: {temp_alta}");
-
-    foreach (int tempu in temperaturas)
-    {
-        if (tempu < temp_baja)
-        {
-            temp_baja = tempu;
-        }
-    }
-    Console.WriteLine($"La temperatura mas baja registrada es: {temp_baja}");
-}
-ejercicio4();
-
-void ejercicio5()
-{
-    int[] desordenados = { 38, 42, 31, 40, 35 };
-
-    Console.WriteLine("Ordenados de forma ascendente queda tal que asi: ");
-    for (int pasada = 0; pasada < desordenados.Length; pasada++)
-    {
-        for (int u = 0; u < desordenados.Length - 1; u++)
-        {
-            if (desordenados[u] > desordenados[u + 1])
+            for (int columna = 0; columna < matrizVisual.GetLength(1); columna++)
             {
-                int temporal = desordenados[u];
-                desordenados[u] = desordenados[u + 1];
-                desordenados[u + 1] = temporal;
+                matrizVisual[fila, columna] = "?" + " ";
+            }
+        }
+
+        int[,] matriz1 = new int[10, 10];
+
+        int unos = 0;
+
+        int intentos = 5;
+
+        bool adivinar = true;
+
+        int adivinados = 0;
+
+        int ultimaX = -1;
+        int ultimaY = -1;
+
+        Random num = new Random();
+
+        while (unos < 3)
+        {
+            int fila = num.Next(0, 10);
+            int columna = num.Next(0, 10);
+
+            if (matriz1[fila, columna] == 0)
+            {
+                matriz1[fila, columna] = 1;
+                unos++;
+            }
+        }
+
+        for (int fila = 0; fila < matriz1.GetLength(0); fila++)
+        {
+            for (int columna = 0; columna < matriz1.GetLength(1); columna++)
+            {
+                Console.Write("?" + " ");
+            }
+            Console.WriteLine();
+        }
+
+        while (adivinar == true)
+        {
+            Console.WriteLine("Elija la casilla en la cordenada x del 1 al 10");
+            int corx = Convert.ToInt32(Console.ReadLine()) - 1;
+
+            Console.WriteLine("Elija la casilla en la cordenada y del 1 al 10(ya ingreso la cordenada en x)");
+            int cory = Convert.ToInt32(Console.ReadLine()) - 1;
+
+            ultimaX = corx;
+            ultimaY = cory;
+
+            if (matriz1[corx, cory] == 1)
+            {
+                Console.WriteLine("Usted acerto");
+                adivinados++;
+
+                matrizVisual[corx, cory] = "O";
+
+                for (int fila = 0; fila < matrizVisual.GetLength(0); fila++)
+                {
+                    for (int columna = 0; columna < matrizVisual.GetLength(1); columna++)
+                    {
+                        Console.Write(matrizVisual[fila, columna] + " ");
+                    }
+                    Console.WriteLine();
+                }
+
+                if (adivinados == 3)
+                {
+                    Console.WriteLine("Usted gano el juego Acerto Todas las posiciones, la matriz usada fue: ");
+
+                    for (int fila = 0; fila < matriz1.GetLength(0); fila++)
+                    {
+                        for (int columna = 0; columna < matriz1.GetLength(1); columna++)
+                        {
+                            Console.WriteLine(matriz1[fila, columna] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                    adivinar = false;
+                    break;
+                }
+            }
+            else
+            {
+                intentos--;
+                Console.WriteLine($"Usted no acerto tiene {intentos} intentos");
+
+                matrizVisual[corx, cory] = "X";
+
+                for (int fila = 0; fila < matrizVisual.GetLength(0); fila++)
+                {
+                    for (int columna = 0; columna < matrizVisual.GetLength(1); columna++)
+                    {
+                        Console.Write(matrizVisual[fila, columna] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                if (intentos == 0)
+                {
+                    Console.WriteLine("Perdio, las casillas eran: ");
+
+                    for (int fila = 0; fila < matriz1.GetLength(0); fila++)
+                    {
+                        for (int columna = 0; columna < matriz1.GetLength(1); columna++)
+                        {
+                            Console.Write(matriz1[fila, columna] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                    adivinar = false;
+                    break;
+                }
             }
         }
     }
-
-    foreach (int numero in desordenados)
-    {
-        Console.Write(numero + " ");
-    }
+    programa6();
 }
-ejercicio5();
-
-void ejercicio6()
+catch (System.FormatException)
 {
-    int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-
-    int pares = 0;
-    int impares = 0;
-
-    Console.WriteLine("Lista");
-    foreach (int nu in numeros)
-    {
-        Console.WriteLine(nu);
-    }
-
-    foreach (int num in numeros)
-    {
-        if (num % 2 == 0)
-        {
-            pares++;
-        }
-        else
-        {
-            impares++;
-        }
-    }
-    Console.WriteLine($"Cant numeros pares: {pares}");
-
-    Console.WriteLine($"Cant numeros impares: {impares}");
+    Console.WriteLine("No puede");
 }
-ejercicio6();
+catch (IndexOutOfRangeException)
+{
+    Console.WriteLine("No puede");
+}
