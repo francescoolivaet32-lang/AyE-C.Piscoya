50fe949195427194d8fc8a3c23ae0b3c6af905f8
francescoolivaet32-lang <francesco.olivaet32@gmail.com>
2026-08-13 11:33:47 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,154 +1,141 @@
-﻿bool ejercicio1(int n)
+﻿using System.ComponentModel.Design;
+using System.Runtime.ConstrainedExecution;
+using System;
+namespace ConsoleApp1
 {
-
-    if (n == 0)
+    public struct Personaje
     {
+        public String Nombre { get; set; }
+        public int Poder { get; set; }
 
-        return false;
+        public String[] Items { get; set; }
+
+        public Personaje(string nombre, int poder)
+        {
+            Nombre = nombre;
+            Poder = poder;
+            Items = new string[20];
+        }
     }
-    else
+
+    public struct Salas
     {
-        ejercicio1(n - 1);
-        Console.WriteLine(n);
-        return true;
-    }
+        public int Dificultad { get; set; }
+        public string Item { get; set; }
 
+        public int Poderitem { get; set; }
+
+        public Salas(int dificultad, string item, int poderitem)
+        {
+            Dificultad = dificultad;
+            Item = item;
+            Poderitem = poderitem;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] objetos = { "Espada de Hierro", "Armadura de Cuero", "Poción de Vida", "Amuleto Mágico", "Escudo Pesado", "Botas de Velocidad", "Casco de Bronce", "Báculo Sagrado", "Anillo de Poder", "Daga Envenenada", "Manto de Invisibilidad", "Poción de Fuerza", "Hacha de Batalla", "Guanteletes de Hierro", "Talismán de la Suerte", "Arco Largo", "Poción de Maná", "Capa del Errante", "Lanza de Plata", "Cinturón de Gigante", "Grimorio Oscuro", "Escudo de Madera" };
+            Random aleatorio = new Random();
+            Personaje marcos = new Personaje("Guerrero", 50);
+            Personaje pedro = new Personaje("Magordito", 60);
+
+            bool turno1 = true;
+            Salas[] listaSalas = new Salas[20];
+            for (int k = 0; k < listaSalas.Length; k++)
+            {
+                string objeto = objetos[aleatorio.Next(0, objetos.Length)];
+                int poderitem = aleatorio.Next(10, 31);
+                listaSalas[k] = new Salas(aleatorio.Next(10, 80), objeto, poderitem);
+            }
+
+            for (int i = 0; i < listaSalas.Length; i++)
+            {
+                Console.WriteLine($"Turno : {i}");
+                Salas salaActual = listaSalas[i];
+                if (turno1 == true)
+                {
+                    if (salaActual.Dificultad < marcos.Poder)
+                    {
+                        Console.WriteLine("Logro robarlo ya que su poder es mayor al de la sala");
+                        marcos.Poder += salaActual.Poderitem;
+                        for (int j = 0; j < marcos.Items.Length; j++)
+                        {
+                            if (marcos.Items[j] == null)
+                            {
+                                marcos.Items[j] = salaActual.Item;
+                                Console.WriteLine($"[{salaActual.Item}] guardado en la mochila de {marcos.Nombre}.");
+                                break;
+                            }
+                        }
+                    }
+                    else { Console.WriteLine("No logro robarlo ya que su poder es menor al de la sala ;("); }
+                    turno1 = false;
+                }
+
+                else
+                {
+                    if (salaActual.Dificultad < pedro.Poder)
+                    {
+                        Console.WriteLine("Logro robarlo ya que su poder es mayor al de la sala");
+                        pedro.Poder += salaActual.Poderitem;
+                        for (int j = 0; j < pedro.Items.Length; j++)
+                        {
+                            if (marcos.Items[j] == null)
+                            {
+                                pedro.Items[j] = salaActual.Item;
+                                Console.WriteLine($"[{salaActual.Item}] guardado en la mochila de {pedro.Nombre}.");
+                                break;
+                            }
+                        }
+                    }
+                    else { Console.WriteLine("No logro robarlo ya que su poder es menor al de la sala ;("); }
+                    turno1 = true;
+                }
+            }
+            if (pedro.Poder < marcos.Poder)
+            {
+                Console.WriteLine($"Ha ganado Cabral - Inventario: ");
+                for (int j = 0; j < marcos.Items.Length; j++)
+                {
+                    if (marcos.Items[j] != null)
+                    {
+                        Console.WriteLine($"Items: [{marcos.Items[j]}]");
+                    }
+                }
+            }
+            else if (pedro.Poder > marcos.Poder)
+            {
+                Console.WriteLine($"Ha ganado Toledo - Inventario: ");
+                for (int j = 0; j < marcos.Items.Length; j++)
+                {
+                    if (pedro.Items[j] != null)
+                    {
+                        Console.WriteLine($"Items: [{pedro.Items[j]}]");
+                    }
+                }
+            }
+            else if (pedro.Poder == marcos.Poder)
+            {
+                Console.WriteLine("Han empatado");
+                Console.WriteLine("Inventario de Cabral");
+                for (int j = 0; j < marcos.Items.Length; j++)
+                {
+                    if (marcos.Items[j] != null)
+                    {
+                        Console.WriteLine($"Items: [{marcos.Items[j]}]");
+                    }
+                }
+                Console.WriteLine("Inventario de Toledo");
+                for (int j = 0; j < pedro.Items.Length; j++)
+                {
+                    if (pedro.Items[j] != null)
+                    {
+                        Console.WriteLine($"Items: [{pedro.Items[j]}]");
+                    }
+                }
+            }
+        }
+    }
 }
-ejercicio1(10);
-
-bool ejercicio2(int n)
-{
-
-    if (n == 0)
-    {
-
-        return false;
-    }
-    else
-    {
-
-        Console.WriteLine("¡Hola mundo!");
-        ejercicio2(n - 1);
-        return true;
-    }
-}
-ejercicio2(5);
-bool ejercicio3(int n)
-{
-    if (n > 20)
-    {
-        return false;
-    }
-    else
-    {
-
-        Console.WriteLine(n);
-        ejercicio3(n + 2);
-        return true;
-    }
-}
-ejercicio3(2);
-bool ejercicio4(int n)
-{
-    if (n > 70)
-    {
-        return false;
-    }
-    else
-    {
-        Console.WriteLine(n);
-        ejercicio4(n + 7);
-        return true;
-    }
-}
-ejercicio4(7);
-int aux = 0;
-int ejercicio5(int n)
-{
-    if (n == 0)
-    {
-        return aux;
-    }
-    else
-    {
-        aux = aux + n;
-        ejercicio5(n - 1);
-        return aux;
-    }
-}
-
-aux = ejercicio6(Convert.ToInt32(Console.ReadLine()));
-Console.WriteLine(aux);
-string ejercicio6(string contra)
-{
-    string secreta = "caine";
-
-    if(secreta == contra)
-    {
-        Console.WriteLine("La contraseña es correcta");
-        return contra;
-    }
-    else
-    {
-        Console.WriteLine("Intentelo de nuevo");
-        ejercicio6(Console.ReadLine());
-        return contra;
-
-    }
-}
-ejercicio6(Console.ReadLine());
-
-int ejercicio7(int despegar)
-{
-    if (despegar == 0)
-    {
-        Console.WriteLine("Listos para despegar");
-        Console.WriteLine("Despegue");
-        return despegar;
-    }
-    else
-    {
-        despegar = despegar - 1;
-        Console.WriteLine(despegar);
-        ejercicio7(despegar);
-        return despegar;
-    }
-}
-ejercicio7(6);
-int ejercicio8(int maquina)
-{
-    Console.WriteLine("Adivine el numero");
-    int numero = Convert.ToInt32(Console.ReadLine());
-    if (numero == maquina)
-    {
-        Console.WriteLine("¡Felicidades! Adivinaste el número.");
-        return maquina;
-    }
-    else
-    {
-        Console.WriteLine("intentelo de vuelta");
-        ejercicio8(maquina);
-        return maquina;
-    }
-}
-ejercicio8(7);
-int acum = 0;
-int ejercicio9()
-{
-    Console.WriteLine("Ingrese un numero");
-    int num = Convert.ToInt32(Console.ReadLine());
-    if(num == 0)
-    {
-        Console.WriteLine("La suma de los numeros es: " + acum);
-        return acum;
-    }
-    else
-    {
-        acum = acum + num;
-        Console.WriteLine("Ingrese otro numero");
-        ejercicio9();
-    }
-    return acum;
-
-}
-ejercicio9();
